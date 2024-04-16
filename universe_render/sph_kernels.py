e075c4292e7479c203ea696e652c6e147b7208d6
pointee <pointee@example.com>
2024-04-16 14:38:08 +0900

    add hinv option

--- universe_render/sph_kernels.py
+++ universe_render/sph_kernels.py
@@ -24,3 +24,19 @@ def cubic_spline_2D(r, h):
     else:
         val = 1 - 1.5 * q * q * (1 - 0.5 * q)
     return val * fac
+
+@jit(nopython=True)
+def cubic_spline_2D_hinv(r, hinv):
+    """
+    2D cubic spline kernel. Taken from https://pysph.readthedocs.io/en/latest/reference/kernels.html
+    """
+    q = r * hinv
+    if q > 2.0:
+        return 0.
+    
+    fac = 10. * np.pi / 7.0 * hinv * hinv
+    if q > 1.0:
+        val = 0.25 * (2. - q) * (2. - q) * (2. - q)
+    else:
+        val = 1 - 1.5 * q * q * (1 - 0.5 * q)
+    return val * fac
